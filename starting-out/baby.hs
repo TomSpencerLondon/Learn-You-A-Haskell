09631f3cf43b2ab08654113ae6a7ed7e6d9e1770
Tom <thomas.spencer@codurance.com>
2021-05-04 17:12:43 +0100

    add baby

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -1,3 +1,7 @@
 doubleMe x = x + x
 
-doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe 2 + doubleMe 2
+
+doubleSmallNumber x = if x > 100
+                         then x
+                         else x*2
