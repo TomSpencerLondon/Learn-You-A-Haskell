8c5a887acb46134d47f3de00543eaff6fa14b55d
Tom <thomas.spencer@codurance.com>
2021-05-04 17:14:23 +0100

    refactor doubleSmallNumber

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -2,6 +2,4 @@ doubleMe x = x + x
 
 doubleUs x y = doubleMe 2 + doubleMe 2
 
-doubleSmallNumber x = if x > 100
-                         then x
-                         else x*2
+doubleSmallNumber x = (if x > 100 then x else x*2) + 1
