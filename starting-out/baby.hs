412788146ae586372abb10f6ed721c33074e4f2b
thomas.spencer@codurance.com <thomas.spencer@codurance.com>
2021-05-08 13:35:22 +0100

    add quicksort haskell function

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -5,3 +5,12 @@ doubleUs x y = doubleMe 2 + doubleMe 2
 doubleSmallNumber x = (if x > 100 then x else x*2) + 1
 
 conanO'Brien = "It's a-me, conan O'Brien!"
+
+qsort [] = []
+qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+                where
+                    smaller = [a | a <- xs, a <= x]
+                    larger = [b | b <- xs, b > x]
+
+
+
