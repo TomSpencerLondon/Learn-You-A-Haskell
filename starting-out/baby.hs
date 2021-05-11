d48e34f2506c70982391251e65331053af825bfd
Tom <thomas.spencer@codurance.com>
2021-05-11 17:42:25 +0100

    Add bmi calculation for calcBmisFat

--- starting-out/baby.hs
+++ starting-out/baby.hs
@@ -58,12 +58,14 @@ sum' :: (Num a) => [a] -> a
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
 
-bmiTell :: (RealFloat a) => a -> String  
-bmiTell bmi  
+bmiTell :: (RealFloat a) => a -> a -> String  
+bmiTell weight height   
     | bmi <= 18.5 = "You're underweight, you emo, you!"  
     | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
     | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
     | otherwise   = "You're a whale, congratulations!"  
+    where bmi = weight / height ^ 2
+
 
 max' :: (Ord a) => a -> a -> a  
 max' a b   
@@ -76,3 +78,26 @@ a `myCompare` b
     | a > b = GT
     | a == b = EQ
     | otherwise = LT
+
+initials :: String -> String -> String  
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."  
+    where (f:_) = firstname  
+          (l:_) = lastname
+
+
+calcBmis :: (RealFloat a) => [(a, a)] -> [a]  
+calcBmis xs = [bmi w h | (w, h) <- xs]  
+    where bmi weight height = weight / height ^ 2
+
+
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea
+
+calcBmisFat :: (RealFloat a) => [(a, a)] -> [a]
+calcBmisFat xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi >= 25.0]
+
+
+
