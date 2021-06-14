b7ebb01b8a13e2109a975b4220a60c70352bc8ff
Tom <thomas.spencer@codurance.com>
2021-06-14 18:25:30 +0100

    add module

--- starting-out/modules.hs
+++ starting-out/modules.hs
@@ -1,4 +1,13 @@
+import Data.List (nub, sort)  
+import Data.List hiding (nub)  
+import qualified Data.Map
+import qualified Data.Map as M
+
 import Data.List
+:m + data.list
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+find (>4) [1, 2, 3, 4]
+
