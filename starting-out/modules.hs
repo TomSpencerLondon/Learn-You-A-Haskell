a604cb6de77ebe7cc1ac757aaebbed3882874413
Tom <thomas.spencer@codurance.com>
2021-06-21 19:03:13 +0100

    add more functions in starting-out

--- starting-out/modules.hs
+++ starting-out/modules.hs
@@ -1,7 +1,11 @@
-import Data.List (nub, sort)  
-import Data.List hiding (nub)  
+import Data.List (nub, sort)
+import Data.List hiding (nub)
+import Data.Char
+import Data.Function
 import qualified Data.Map
-import qualified Data.Map as M
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
 
 import Data.List
 
@@ -12,3 +16,41 @@ search :: (Eq a) => [a] -> [a] -> Bool
 search needle haystack =
     let nlen = length needle
     in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+
+
+encode :: Int -> String -> String
+encode shift msg =
+    let ords = map ord msg
+        shifted = map (+ shift) ords
+    in map chr shifted
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
+
+phoneBook =   
+    [("betty","555-2938")  
+    ,("bonnie","452-2928")  
+    ,("patsy","493-2928")  
+    ,("lucille","205-2928")  
+    ,("wendy","939-8282")  
+    ,("penny","853-2492")  
+    ]
+
+-- findKey :: (Eq k) => k -> [(k,v)] -> v  
+-- findKey key xs = snd . head . filter (\(k,v) -> key == k) $ xs
+
+
+findKey :: (Eq k) => k -> [(k,v)] -> Maybe v  
+findKey key [] = Nothing  
+findKey key ((k,v):xs) = if key == k  
+                            then Just v  
+                            else findKey key xs 
+
+fromList' :: (Ord k) => [(k,v)] -> Map.Map k v
+fromList' = foldr (\(k,v) acc -> Map.insert k v acc) Map.empty
+
+
+text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
+text2 = "The old man left his garbage can out and now his trash is all over my lawn!"
+
+
