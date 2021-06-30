be085886686762218cea0f16424f2579d6324074
Tom <thomas.spencer@codurance.com>
2021-06-30 23:27:11 +0100

    add types

--- types-and-typeclasses/types.hs
+++ types-and-typeclasses/types.hs
@@ -10,3 +10,19 @@ surface :: Shape -> Float
 surface (Circle _ r) = pi * r ^ 2
 surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
+
+-- data Car = Car String String Int deriving (Show)
+
+data Car = Car {company :: String, model :: String, year :: String} deriving (Show)
+
+data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday   
+           deriving (Eq, Ord, Show, Read, Bounded, Enum)  
+
